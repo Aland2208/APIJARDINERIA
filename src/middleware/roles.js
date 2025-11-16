e27eb648b0e1e962e6b06e40a46900cbabb6b06f
danieltesis232004 <danieltesis232004@gmail.com>
2025-11-15 23:21:59 -0500

    jardineros

--- src/middleware/roles.js
+++ src/middleware/roles.js
@@ -1,6 +1,6 @@
 export const permitirRol = (...rolesPermitidos) => (req, res, next) => {
-  const { rol } = req.usuario; 
-  if (!rolesPermitidos.includes(rol)) {
+  const { tipo } = req.usuario; 
+  if (!rolesPermitidos.includes(tipo)) {
     return res.status(403).json({ mensaje: 'No tienes permisos' });
   }
   next();
