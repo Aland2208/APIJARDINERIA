fa0a0f2d74dd51d8c86c927ae5ed6e016c87a3c4
danieltesis232004 <danieltesis232004@gmail.com>
2025-11-15 23:16:08 -0500

    cambios

--- src/middleware/roles.js
+++ src/middleware/roles.js
@@ -1,5 +1,5 @@
 export const permitirRol = (...rolesPermitidos) => (req, res, next) => {
-  const { rol } = req.usuario; // se supone que verifyToken agrega `req.usuario`
+  const { rol } = req.usuario; 
   if (!rolesPermitidos.includes(rol)) {
     return res.status(403).json({ mensaje: 'No tienes permisos' });
   }
