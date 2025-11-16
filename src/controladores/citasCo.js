23d8dfc7955f35f419c5479bef5c46922b729415
danieltesis232004 <danieltesis232004@gmail.com>
2025-11-16 00:06:24 -0500

    cambios

--- src/controladores/citasCo.js
+++ src/controladores/citasCo.js
@@ -120,6 +120,42 @@ export const actualizarCita = async (req, res) => {
     res.status(500).json({ mensaje: 'Internal server error' });
   }
 };
+export const getCitasPorJardinero = async (req, res) => {
+  try {
+    const { id_jardinero } = req.params; // Obtenemos el id del jardinero de la URL
+    if (!id_jardinero) {
+      return res.status(400).json({ mensaje: 'Se requiere id_jardinero' });
+    }
 
+    // Traemos solo citas pendientes asignadas al jardinero
+    const [result] = await conmysql.query(
+      'SELECT * FROM Citas WHERE id_jardinero_asignado = ? AND estado = "pendiente"',
+      [id_jardinero]
+    );
+
+    res.json({ cantidad: result.length, data: result });
+  } catch (error) {
+    console.error('Error en getCitasPorJardinero:', error);
+    res.status(500).json({ mensaje: 'Internal server error' });
+  }
+};
+ export const getCitasPorJardineroConf = async (req, res) => {
+  try {
+    const { id_jardinero } = req.params; // Obtenemos el id del jardinero de la URL
+    if (!id_jardinero) {
+      return res.status(400).json({ mensaje: 'Se requiere id_jardinero' });
+    }
+
+    const [result] = await conmysql.query(
+      'SELECT * FROM Citas WHERE id_jardinero_asignado = ? AND estado = "aceptada"',
+      [id_jardinero]
+    );
+
+    res.json({ cantidad: result.length, data: result });
+  } catch (error) {
+    console.error('Error en getCitasPorJardinero:', error);
+    res.status(500).json({ mensaje: 'Internal server error' });
+  }
+};
 
 
--- src/routes/citas.routes.js
+++ src/routes/citas.routes.js
@@ -9,7 +9,7 @@ router.get('/citas', verifyToken, getCitas);
 router.post('/citas', verifyToken, permitirRol('cliente'), postCita);
 router.get('/citas/cliente/:id_cliente', getCitasPorCliente);
 router.get('/citas/jardinero/:id_jardinero', verifyToken, getCitasPorJardinero); 
-vrouter.get('/citasConf/jardinero/:id_jardinero', verifyToken, getCitasPorJardineroConf); 
+router.get('/citasConf/jardinero/:id_jardinero', verifyToken, getCitasPorJardineroConf); 
 router.put('/citas/:id_cita/cancelar', verifyToken, permitirRol('cliente', 'jardinero'), cancelarCita);
 router.put('/citas/:id_cita', actualizarCita);
 
