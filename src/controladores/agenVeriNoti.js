24a2e64c8cb02e65b003dd64cc23715ada92ea89
danieltesis232004 <danieltesis232004@gmail.com>
2025-11-17 13:21:31 -0500

    agenda id

--- src/controladores/agenVeriNoti.js
+++ src/controladores/agenVeriNoti.js
@@ -30,10 +30,10 @@ export const postAgenda = async (req, res) => {
 
 export const postVerificacion = async (req, res) => {
     try {
-        const { id_cita, id_jardinero, estado, observaciones, precio_final,fecha_verificacion, hora} = req.body;
+        const { id_cita, id_jardinero, estado, observaciones, precio_final, fecha_verificacion, hora } = req.body;
         const [result] = await conmysql.query(
             'INSERT INTO Verificaciones(id_agenda, id_jardinero, estado, observaciones, precio_final,fecha_verificacion,hora) VALUES (?,?,?,?,?,?,?)',
-            [id_cita, id_jardinero, estado, observaciones, precio_final,fecha_verificacion, hora]
+            [id_cita, id_jardinero, estado, observaciones, precio_final, fecha_verificacion, hora]
         );
         res.json({ mensaje: 'Verificación registrada', id_verificacion: result.insertId });
     } catch (error) {
@@ -171,6 +171,7 @@ export const getNotificacionesPorJardinero = async (req, res) => {
 
         const notificaciones = agendas.map(a => ({
             id_cita: a.id_cita,
+            id_agenda: a.id_agenda,       
             estado: a.estado,
             ubicacion: a.ubicacion,
             referencia: a.referencia,
