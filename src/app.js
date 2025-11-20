be21a218fab25918c4eb9081d95e356581f342cd
Aland Loor <aland2208@gmail.com>
2025-11-20 11:18:15 -0500

    fin5

--- src/app.js
+++ src/app.js
@@ -24,15 +24,13 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cors({
-  origin: ['http://localhost:8100', 'https://apijardineria.onrender.com'],
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  allowedHeaders: ['Content-Type', 'Authorization'],
+// ✅ Configuración de CORS
+const corsOptions = {
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
   credentials: true
-}));
-
-app.options('*', cors());
-
+};
+app.use(cors(corsOptions));
 
 // ✅ Servir archivos estáticos (si más adelante subes imágenes locales)
 app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
